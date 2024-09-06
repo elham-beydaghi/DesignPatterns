618c93a74dce3d6e31b97725ecd7da7cf08e2de4
elham-beydaghi <elhambeydaghi1999@gmail.com>
2024-09-06 16:02:46 +0330

    Add project files.

---
+++ Design Patterns/BehavioralPattern/ChainOfResponsibility.cs
@@ -0,0 +1,56 @@
+﻿namespace Design_Patterns.BehavioralPattern
+{
+    public interface IHandler
+    {
+        IHandler SetNextHandler(IHandler handler);
+
+        object Handle(object request);
+    }
+
+    public abstract class ChainOfResponsibility : IHandler
+    {
+        private IHandler _nextHandler { get; set; }
+        public virtual object Handle(object request)
+        {
+            if (_nextHandler != null)
+            {
+                return _nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IHandler SetNextHandler(IHandler handler)
+        {
+            _nextHandler = handler;
+            return _nextHandler;
+        }
+    }
+
+    public class HandleOne() : ChainOfResponsibility
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "person one")
+            {
+                return "How Are You?";
+            }
+            return base.Handle(request);
+        }
+    }
+
+    public class HandleTwo() : ChainOfResponsibility
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "person two")
+            {
+                return "Thanks";
+            }
+            return base.Handle(request);
+        }
+    }
+
+}
---
+++ Design Patterns/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Design_Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var handleOne = new HandleOne();
+            //var y = handleOne.Handle("person one");
+            //Console.WriteLine(y);
+
+
+        }
+    }
+}
